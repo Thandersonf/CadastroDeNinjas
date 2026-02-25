390e99e0771acf2e3f183df5dd4b265ccb0a2855
Thanderson <thandersonf@gmail.com>
2026-02-25 17:08:49 -0300

    primeiro commit

---
+++ src/test/java/de/java10x/CadastroDeNinjas/CadastroDeNinjasApplicationTests.java
@@ -0,0 +1,13 @@
+package de.java10x.CadastroDeNinjas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeNinjasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
