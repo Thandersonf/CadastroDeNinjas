dbfa610a3acf140d2c8d3646a2d50aaa36cea841
Thanderson <thandersonf@gmail.com>
2026-07-30 20:19:58 -0300

    Atualiza o controller com mock de dados

--- src/main/java/de/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/de/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,8 +1,6 @@
 package de.java10x.CadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -12,5 +10,32 @@ public class NinjaController {
         return " Essa é minha primeira mensagem";
 
 
-    }
+        }
+        //Adicionar ninja(CREATE)
+         @PostMapping("/criar")
+         public String criarNinja(@RequestBody NinjaModel ninja) {
+             return "Ninja criado";
+         }
+        //Mostrar todos os Ninjas(READ)
+
+         @GetMapping("/todos")
+         public String mostrarTodosOsNinjas(){
+            return "Mostrar todos os Ninjas";}
+
+        //Mostrar ninjas por ID(READ)
+
+         @GetMapping("/todosID")
+         public String mostrarNinjasPorID(){
+            return "Mostrar Ninja por ID";}
+
+        //Alterar dados do Ninja(UPDATE)
+        @PutMapping("/alterar")
+        public String alterarNinjaPorID(){
+             return "Alterar Ninja por ID";}
+        //Deletar Ninja(DELETE)
+        @DeleteMapping("/deletarId")
+        public String deletarNinjaPorID(){
+            return "Ninja deletado por ID";
 }
+    }
+
